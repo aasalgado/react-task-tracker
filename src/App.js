3e715b88a397e20d07cd3d70fb29aeb20e12f87e
Anthony Salgado <anthonyasalgado@gmail.com>
2021-04-30 11:26:02 -0700

    created  Task and Tasks components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Mar 27th at 3:00pm',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'April 20th at 5:00pm',
+      reminder: true
+    },
+  ])
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
